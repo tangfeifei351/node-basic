2ef3beaaf95a64b14f130019a79343f1cad520ce
Tang <tangfeifei351@126.com>
2020-10-30 16:58:40 +0800

    666

--- http.js
+++ http.js
@@ -2,5 +2,5 @@ const http = require('http');
 http.createServer(function(request,response){
     console.log(request)
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('666');
+    response.end(666);
 }).listen(3000)
